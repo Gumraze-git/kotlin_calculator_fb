284f1fd411558d462ab6998af0a44b10f4a42299
Gumraze-git <galaxydh4110@gmail.com>
2025-02-14 16:18:55 +0900

    test(getSum): 음수 및 숫자가 아닌 문자열 입력 시 예외 처리 테스트 추가
    - 입력 값에 음수가 포함된 경우 `IllegalArgumentException`이 발생하는지 확인하는 테스트 추가
    - 숫자가 아닌 문자열이 포함된 경우 `IllegalArgumentException`이 발생하는지 검증하는 테스트 추가

--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -1,6 +1,7 @@
 package calculator
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.assertj.core.api.Assertions.assertThat
 
 class CalculatorTest {
@@ -13,5 +14,23 @@ class CalculatorTest {
         assertThat(calculator.getSum("1,2")).isEqualTo(3)
         assertThat(calculator.getSum("1,2,3")).isEqualTo(6)
         assertThat(calculator.getSum("1,2:3")).isEqualTo(6)
+    }
+
+    @Test
+    fun `숫자가 아닌 문자 포함 시 예외 발생`() {
+        val calculator = Calculator()
+        val exception = assertThrows<IllegalArgumentException> {
+            calculator.getSum("1,2,a")
+        }
+        assertThat(exception.message).isEqualTo("Invalid number a")
+    }
+
+    @Test
+    fun `음수 입력 시 예외 발생`() {
+        val calculator = Calculator()
+        val exception = assertThrows<IllegalArgumentException> {
+            calculator.getSum("-1,2:3")
+        }
+        assertThat(exception.message).isEqualTo("Invalid number -1")
     }
 }
